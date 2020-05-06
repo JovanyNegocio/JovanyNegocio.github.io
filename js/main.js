08908a69cd25a1eccea8d8b519b4ae0cbbba4e27
Angelino Valeta <angelinovaleta@gmail.com>
2020-05-06 09:24:09 +0100

    show modal

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 const btnHire = document.querySelector('.hero-btn')
 const modal = document.querySelector('.modal')
-const close-modal = document.querySelector('.close-modal')
+const contentModal = document.querySelector('.content-modal')
+const closeModal = document.querySelector('.close-modal')
+
+console.log(contentModal)
 
 btnHire.addEventListener('click', () => {
-
+    //console.log('click')
+    modal.classList.add('show-modal')
+    contentModal.classList.add('show-modal')
+    //console.log(contentModal.getAttribute('class'))
+    //contentModal.style.display = 'block'
 
 
 })
